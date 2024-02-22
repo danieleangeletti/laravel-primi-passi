e0f8a370cc5948f1eb9b61caa0979d7d1f9efe2a
danieleangeletti <angelettidaniele@gmail.com>
2024-02-22 13:16:25 +0100

    esercizio base completato

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Primi Passi</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" />
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <h1>
+                Welcome to my first project with Laravel!
+            </h1>
+        </div>
+    </header>
+</body>
+
+</html>
